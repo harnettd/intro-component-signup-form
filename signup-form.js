703e431df62477f9bffb1e503638aff731b9095f
Derek Harnett <DHarnett.dev@proton.me>
2025-01-11 08:22:54 -0800

    commit changes from waaaay to long ago

--- signup-form.js
+++ signup-form.js
@@ -1,3 +1,11 @@
+const formID = 'signup-form'
+
+const form = document.querySelector(`#${signup-form}`)
+const firstName = document.querySelector('#first-name')
+const lastName = document.querySelector('#last-name')
+const emailAddress = document.querySelector('#email-address')
+const password = document.querySelector('#password')
+
 const getRelatives = (input) => {
   const container = input.parentElement
   const errIcon =  input.nextElementSibling
@@ -5,28 +13,29 @@ const getRelatives = (input) => {
   return {container, errIcon, errMessage}
 }
 
-window.addEventListener('load', () => {
-  const inputs = document.querySelectorAll('.signup-form__input')
-  inputs.forEach((input) => {
-    input.value = ''
-    input.classList.remove('signup-form__input--on-err')
-
-    const {container, errIcon, errMessage} = getRelatives(input)
-    container.classList.remove('signup-form__input-container-flex--on-err')
-    errIcon.classList.remove('signup-form__err-icon--on-err')    
-    errMessage.classList.remove('signup-form__err-msg--on-err')
-  })
-})
-
-const form = document.querySelector('#signup-form')
-
 const addOnErr = (input) => {
   const {container, errIcon, errMessage} = getRelatives(input)
-  input.classList.add('signup-form__input--on-err')
+  input.classList.add(`${formID}__input--on-err`)
   container.classList.add('signup-form__input-container-flex--on-err')
   errIcon.classList.add('signup-form__err-icon--on-err')    
   errMessage.classList.add('signup-form__err-msg--on-err') 
 }
+
+const removeOnErr = (input) => {
+  const {container, errIcon, errMessage} = getRelatives(input)
+  input.classList.remove('signup-form__input--on-err')
+  container.classList.remove('signup-form__input-container-flex--on-err')
+  errIcon.classList.remove('signup-form__err-icon--on-err')    
+  errMessage.classList.remove('signup-form__err-msg--on-err') 
+}
+
+window.addEventListener('load', () => {
+  const inputs = document.querySelectorAll('.signup-form__input')
+  inputs.forEach((input) => {
+    input.value = ''
+    removeOnErr(input)    
+  })
+})
 
 const isValidEmail = (email) => {
   if (email.indexOf('@') === -1) {
@@ -40,11 +49,6 @@ const isValidEmail = (email) => {
 
 form.addEventListener('submit', function(evt) {
   evt.preventDefault()
-
-  const firstName = document.querySelector('#first-name')
-  const lastName = document.querySelector('#last-name')
-  const emailAddress = document.querySelector('#email-address')
-  const password = document.querySelector('#password')
 
   console.log(firstName.value)
   console.log(lastName.value)
