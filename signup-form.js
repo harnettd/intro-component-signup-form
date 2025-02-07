638a6d1166675284451f16ab1faee9493a984210
Derek Harnett <DHarnett.dev@proton.me>
2025-02-06 16:27:02 -0800

    refactor index.js

--- signup-form.js
+++ signup-form.js
@@ -1,8 +1,10 @@
-const formID = "signup-form"; // form id
-const form = document.querySelector(`#${formID}`);
+// Return a class name in the BEM convention.
+const BEM = (block, element, modifier) => {
+  const BE = `${block}__${element}`;
+  return modifier ? `${BE}--${modifier}` : BE;
+}
 
-const formBlock = "signup-form"; // form block (i.e., class)
-// input elements
+const form = document.querySelector("#signup-form");
 const firstName = document.querySelector("#first-name");
 const lastName = document.querySelector("#last-name");
 const emailAddress = document.querySelector("#email-address");
@@ -17,25 +19,31 @@ const getRelatives = (input) => {
   return { container, errIcon, errMessage };
 };
 
-const onErr = "on-err"; //input-error BEM modifier
-const onErrClass = (element) => `${formBlock}__${element}--${onErr}`;
+const formBlock = "signup-form";
+const inputElement = "input";
+const containerElement = "input-container-flex";
+const errIconElement = "err-icon";
+const errMessageElement = "err-msg";
+const onErrModifier = "on-err";
+
+const onErrClass = (element) => BEM(formBlock, element, onErrModifier);
 
 // Add input-error modifiers to all elements associated with an input.
 const addOnErr = (input) => {
   const { container, errIcon, errMessage } = getRelatives(input);
-  input.classList.add(onErrClass("input"));
-  container.classList.add(onErrClass("input-container-flex"));
-  errIcon.classList.add(onErrClass("err-icon"));
-  errMessage.classList.add(onErrClass("err-msg"));
+  input.classList.add(onErrClass(inputElement));
+  container.classList.add(onErrClass(containerElement));
+  errIcon.classList.add(onErrClass(errIconElement));
+  errMessage.classList.add(onErrClass(errMessageElement));
 };
 
 // Remove input-error modifiers from all elements associated with an input.
 const removeOnErr = (input) => {
   const { container, errIcon, errMessage } = getRelatives(input);
-  input.classList.remove(onErrClass("input"));
-  container.classList.remove(onErrClass("input-container-flex"));
-  errIcon.classList.remove(onErrClass("err-icon"));
-  errMessage.classList.remove(onErrClass("err-msg"));
+  input.classList.remove(onErrClass(inputElement));
+  container.classList.remove(onErrClass(containerElement));
+  errIcon.classList.remove(onErrClass(errIconElement));
+  errMessage.classList.remove(onErrClass(errMessageElement));
 };
 
 // On webpage load, clear all input values and remove all error
@@ -50,7 +58,8 @@ window.addEventListener("load", () => {
 // When an input has focus, add a corresponding class to its container.
 // When it loses focus, remove the class.
 inputs.forEach((input) => {
-  const focusClass = `${formBlock}__input-container-flex--subfocus`;
+  // const focusClass = `${formBlock}__input-container-flex--subfocus`;
+  const focusClass = BEM(formBlock, containerElement, "subfocus");
   const container = getRelatives(input).container;
 
   input.addEventListener("focus", () => {
@@ -63,49 +72,41 @@ inputs.forEach((input) => {
   });
 });
 
+// Return true if the value of input is nonempty; false otherwise.
+const isNonempty = (input) => {
+  return input.value.trim() ? true : false;
+}
+
 // Return true if email is a valid email address; false otherwise.
 const isValidEmail = (email) => {
   const emailRegexp = /^\w+(\.\w+)*@\w+(\.\w+)*$/g;
-  if (email.match(emailRegexp) === null) {
-    return false;
-  }
-  return true;
+  return email.value.match(emailRegexp) ? true : false;
 };
 
-// When the form is submitted, perform some client-side input validation.
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-
-  if (firstName.value === "") {
-    addOnErr(firstName);
+// Test an input against a condition and update testResults.
+const test = (input, condition, testResults) => {
+  const result = condition(input);
+  testResults.push(result);
+  if (result) {
+    removeOnErr(input);
   } else {
-    removeOnErr(firstName);
+    addOnErr(input);
   }
+}
 
-  if (lastName.value === "") {
-    addOnErr(lastName);
+// When the form is submitted, perform client-side input validation.
+form.addEventListener("submit", function (event) {
+  const testResults = [];
+
+  test(firstName, isNonempty, testResults);
+  test(lastName, isNonempty, testResults);
+  test(emailAddress, isValidEmail, testResults);
+  test(password, isNonempty, testResults);
+
+  const isValidInputs = testResults.every((x) => x);
+  if (isValidInputs) {
+    alert("All inputs are valid");
   } else {
-    removeOnErr(lastName);
-  }
-
-  if (!isValidEmail(emailAddress.value)) {
-    addOnErr(emailAddress);
-  } else {
-    removeOnErr(emailAddress);
-  }
-
-  if (password.value === "") {
-    addOnErr(password);
-  } else {
-    removeOnErr(password);
-  }
-
-  if (
-    firstName.value !== "" &&
-    lastName.value !== "" &&
-    isValidEmail(emailAddress.value) &&
-    password.value !== ""
-  ) {
-    console.log("All inputs are valid.");
+    event.preventDefault();
   }
 });
